f34794bded8fd333624bb1151f819567d0a3e66f
Kharold Canova <kharoldsamir@gmail.com>
2021-12-20 21:15:12 -0500

    configuracion de la voz

--- jarvis.py
+++ jarvis.py
@@ -1,11 +1,16 @@
 import speech_recognition as sr
 import pyttsx3
 
+#instancias
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
+#motor de voz
 engine.say("Buenos dias")
 engine.runAndWait()
+#elegir otra voz
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
 
 try:
     with sr.Microphone() as source:
