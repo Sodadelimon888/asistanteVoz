a2a45b2e2f1a196dc11b9a2cd7cfee5fbd7b5411
Kharold Canova <kharoldsamir@gmail.com>
2021-12-20 21:50:48 -0500

    solo responde ante el nombre

--- jarvis.py
+++ jarvis.py
@@ -1,22 +1,28 @@
 import speech_recognition as sr
 import pyttsx3
 
+# nombre del asistente
+name = 'samir'
 #instancias
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
 #motor de voz
-engine.say("Buenos dias")
-engine.runAndWait()
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
 #elegir otra voz
 voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[1].id)
+engine.setProperty("voice", voices[2].id)
 
 try:
     with sr.Microphone() as source:
         print ("Escuchando consulta ...")
         voice = listener.listen(source)
         rec = listener.recognize_google(voice)
-        print(rec)
+        rec = rec.lower()
+        if name in rec:
+            talk(rec)
 except:
     pass
