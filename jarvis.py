db2e62bd41864b84f8199359a16fc12cbadbcc62
Sodadelimon888 <kharoldamir@gmail.com>
2022-03-21 20:25:05 -0500

    "subiendo a repositorio"

--- jarvis.py
+++ jarvis.py
@@ -1,11 +1,16 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit 
+
 
 # nombre del asistente
 name = 'samir'
 #instancias
 listener = sr.Recognizer()
 engine = pyttsx3.init()
+
+engine.say("Buenos dias")
+engine.runAndWait()
 
 #motor de voz
 def talk(text):
@@ -16,13 +21,26 @@ def talk(text):
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[2].id)
 
-try:
-    with sr.Microphone() as source:
-        print ("Escuchando consulta ...")
-        voice = listener.listen(source)
-        rec = listener.recognize_google(voice)
-        rec = rec.lower()
-        if name in rec:
-            talk(rec)
-except:
-    pass
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print ("Escuchando consulta ...")
+            voice = listener.listen(source)
+            rec = listener.recognize_google(voice)
+            rec = rec.lower()
+            #reconoce el nombre en la grabacion
+            if name in rec:
+                #quita el nombre en la grabacion
+                rec = rec.replace(name, "")
+                print(rec)
+    except:
+        pass
+    return rec
+
+""" def run ():
+    rec = listen()
+    if 'reproduce' in rec:
+        music = rec.replace("reproduce", "")
+        talk("Reproduciendo" + music)
+        pywhatkit .playonyt(music)
+run() """
